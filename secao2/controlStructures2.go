322b106808e1f3ffd405bc82b4a07daa48a633b4
AllenLVieira <allenvieira96@gmail.com>
2022-05-25 23:26:15 -0300

    feat: Uso do break

--- secao2/controlStructures2.go
+++ secao2/controlStructures2.go
@@ -9,4 +9,14 @@ func main() {
 		}
 		fmt.Println(i)
 	}
+
+	marcador := true
+	for i := 1; i < 10; i++ {
+		if i%2 == 0 {
+			marcador = false
+			break
+		}
+		fmt.Println(i)
+	}
+	fmt.Println(marcador)
 }
